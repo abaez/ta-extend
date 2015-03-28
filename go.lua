84b1b903eaca5fb8fc736cb44b73d774474592fb
Alejandro Baez <Alejan.Baez@gmail.com>
2015-03-28 12:23:22 -0400

    adding go, lua

---
+++ go.lua
@@ -0,0 +1,40 @@
+--- extends go.
+-- @author [Alejandro Baez](https://twitter.com/a_baez)
+-- @coypright 2015
+-- @license MIT (see LICENSE)
+-- @module go
+
+local snippets = {
+  const = 'const %1(name) %2(type)  = %3(value)',
+
+  -- functions
+  fn = 'func %1(name)(%2(arguments))%3( (%4(results))) {\n\t%0\n}',
+  fna = 'func(%1(arguments))%2( (%3(results))) {\n\t%0\n}',
+  fnp = 'func (%1(*T)) %2(name)(%3(arguments))%4( (%5(results))) {\n\t%0\n}',
+
+  imp = 'import %1((\n\t"%2(package)"%0\n))',
+  pkg = 'package %1(main)',
+  var = 'var %1(name) %2(type) %3( = %4(value))',
+
+  -- type calls
+  ["type"] = 'type %1(Name) %2(struct) {\n\t%0\n}',
+
+  -- conditionals
+  ["for"] = "for %1(name) := %2(0); %1 %3(<) %4(10); %1%5(++) {\n\t%0\n}",
+  ["forw"] = "for %1(name) %2(<) %3(condition) {\n\t%0\n}",
+  ["forr"] = "for %1(name), %2(tmp) := range %4(s_m) {\n\t%0\n}",
+
+  -- if block
+  ["if"] = "if %1(statement) {\n\t%2\n}%0",
+  ["ife"] = "if %1(statement) {\n\t%2(body)\n} else %3({\n\t%0\n})",
+  ["else"] = "else {\n\t%0\n}",
+    ["switch"] = "switch %1(statement) {\n%2\ndefault:\n\t%0\n}",
+    ["case"] = "case %1(name):\n\t%2(block)\n%0",
+
+    -- random
+    ['select'] = "select {\n\t%0\n}",
+}
+
+return {
+  snippets = snippets
+}
---
+++ lua.lua
@@ -0,0 +1,45 @@
+--- lua extensions
+-- @author [Alejandro Baez](https://twitter.com/a_baez)
+-- @coypright 2015
+-- @license MIT (see LICENSE)
+-- @module lua
+
+--- settings to enable on connect
+buffer.tab_width = 2
+buffer.use_tabs = false
+
+
+local snippets = {
+  -- loops
+  foriter = "for %1(k) in %2(iterator) do\n\t%0\nend",
+  fori = "for %1(i), %2(v) in ipairs(%3(table)) do\n\t%0\nend",
+  forp = "for %1(k), %2(v) in pairs(%3(table)) do\n\t%0\nend",
+  ["for"] = "for %1(i) = %2(1), %3(10)%4(, -1) do\n\t%0\nend",
+  ["while"] = "while %1(exp) do\n\t%0\nend",
+  ["repeat"] = "repeat\n\t%0\nuntil %1(exp) end",
+  ["do"] = "do\n\t%0\nend",
+
+  -- functions
+  ["fnc"] = "%3(--- %4\n-- @function %1\n-- @param %2)\nfunction %1(name)(%2(param))\n\t%0\nend",
+  ["fn"] = "function %1(name)(%2(param))\n\t%0\nend",
+  ["fna"] = "function(%1(param))\n\t%0\nend",
+  ["l"] = "function(%1(param)) return %0 end",
+
+  -- Miscelenious
+  ["local"] = "local %1(x) = %2(value)",
+  ["--"] = "--[[\n\t%0\n--]]",
+  ["---"] = "---[[ %1(name)\n%0\n--]]",
+  ["b"] = "%1(describe)(\"%2\", function()\n\t%0\nend)",
+  ["r"] = 'require("%1(module)")%0',
+  ["c"] = '%1(name)("%2(str)", %3(fna))',
+
+  -- if statement
+  ["if"] = "if %1(condition) then\n\t%2\n%3(end)",
+  ["ife"] = "if %1(condition) then\n\t%2(--body)\nelse\n\t%0\nend",
+  ["else"] = "else\n\t%0\nend",
+  ["elseif"] = "elseif %1(condition) then\n\t%2\n%3(end)",
+}
+
+return {
+  snippets = snippets
+}
