23ee64fb1b4592d07ffaeac2dbc43ade1e182999
Alejandro Baez <Alejan.Baez@gmail.com>
2015-04-22 17:07:44 -0400

    yaml: added loops

--- yaml/post_init.lua
+++ yaml/post_init.lua
@@ -8,8 +8,15 @@ local snippets_list = {
   ["-"]     = "- name: %1(description)\n\t%2(module): %0",
   ["---"]   = "---\n- name: %0",
   ["f"]     = "\t%1(param)=%0",
-  ["n"]     = "%1(param)=%0",
+  ["i"]     = "%1(param)=%0",
+
+  -- loops
   ["wi"]     = "with_items:\n\t- %1\n\t- %0",
+  ["wn"]     = "with_nested:\n\t%0",
+  ["ws"]     = "with_sequence: %0",
+
+  -- if
+  ['if']    = "when: %1(cond)",
 }
 
 if type(snippets) == 'table' then
