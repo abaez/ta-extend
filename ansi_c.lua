c756b1af6e633f59d98262201b3a8a1348746776
Alejandro Baez <code@baez.nyc>
2016-01-28 17:49:01 -0500

    ansi_c: adding snippet_list instead

--- ansi_c.lua
+++ ansi_c.lua
@@ -4,6 +4,31 @@
 -- @license MIT (see LICENSE)
 -- @module ansi_c
 
+snippet_list = {
+  -- functions and structs
+  fn = '%1(T) %2(name)(%3(arguments)) \n{\n\t%0\n}',
+  struct = "struct %1(name) {\n\t%0\n} %2( object_name);",
+  enum = "enum %1(name) {\n\t%0\n}",
+  t = "typedef %1(type) %2(alias);%0",
+
+  -- random
+  ['in'] = '#include %1(<%0>)',
+  ['printf'] = 'printf("%1", %2(param))',
+  ['/*'] = "/*\n * %0\n */",
+  ["d"] = "(%1(T) *)malloc(sizeof(%1));%0",
+
+  -- conditional
+  ['for'] = 'for (%1(i) = %2(0); %1 %3(<) %4(10); %5(%1++)) {\n\t%0\n}',
+  ['while'] = 'while (%1(expr)) {\n\t%0\n}',
+  ['do'] = 'do {\n\t%1\n} while (%2); %0',
+
+  -- if
+  ['if'] = 'if (%1(expr)) {\n\t%2\n} %0',
+  ['else'] = 'else {\n\t%0\n}',
+  ['switch'] = 'switch (%1(expr)) {\n%1\ndefault:\n\t%0\n}',
+  ['case'] = 'case %1(expr):\n\t%2\n%0',
+}
+
 local function connecting()
   --- settings to enable on connect
   buffer.tab_width  = 2
@@ -11,7 +36,7 @@ local function connecting()
 end
 
 local function snipping()
-  snippets.ansi_c = require("linux.snippets")
+  snippets.ansi_c = snippet_list
 end
 
 return {
