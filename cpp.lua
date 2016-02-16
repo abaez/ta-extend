e2282293df29e8c415976a7edbb7e3deffa2a28c
Alejandro Baez <code@baez.nyc>
2016-02-16 15:42:42 -0500

    cpp: changed how fn and brackets expanded

--- cpp.lua
+++ cpp.lua
@@ -5,22 +5,26 @@
 -- @module cpp
 
 local snippets_list = {
-  fn = '%1(T) %2(name)(%3(arguments)) \n{\n\t%0\n}',
-  struct = "struct %1(type_name) {\n\t%0\n} %2( object_name);",
-  enum = "enum %1(type_name) {\n\t%0\n} %2( object_name);",
+  -- functions and structs
+  fn = '%1(T) %2(name)(%3(arguments))%4(;)',
+  struct = "struct %1(name) %2(;)",
+  enum = "enum %1(name) %2(;)",
+  ["type"] = "typedef %1(type) %2(alias);%0",
   union = "union %1(type_name) {\n\t%0\n} %2( object_name);",
   template = "template <class %1(T)>\n%1 %2(name)(%3param) \n{\n\t%0\n}",
-  t = "template <typename %1(T)>\n%0",
+  tem = "template <typename %1(T)>\n%0",
 
   -- class details
-  class = "class %1(name) {\n\t%0\n}%2( object_names);",
+  class = "class %1(name) %2(;)",
   public = "public:\n\t%0",
   private = "private:\n\t%0",
   protected = "protected:\n\t%0",
 
   -- random
-  ['in'] = '#include %1(<%0>)',
+  ['in'] = '#include %1(<%2(name)>)%0',
   ['/*'] = "/*\n * %0\n */",
+  ['{'] = "{\n\t%0\n",
+  ['def'] = '#ifndef %1(name)\n#define %1\n%0\n#endif // %1',
 
   -- conditional
   ['for'] = 'for (%1(i) = %2(0); %1 %3(<) %4(10); %5(%1++)) {\n\t%0\n}',
